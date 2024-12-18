18f1c44b15ec48777dabd65d6509712d133dabc5
chanweiyan <chanweiyan007@gmail.com>
2024-12-18 11:55:28 +0800

    fix: useMergeState

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,75 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
+import useMergeState from "./hooks/useMergeState";
 
-interface CalendarProps{
+interface CalendarProps {
+  value?: Date;
+  defaultValue?: Date;
+  onChange?: (date: Date) => void;
+}
+
+interface CalendarProps {
   value?: Date;
   defaultValue?: Date;
   onChange?: (date: Date) => void;
 }
 
 function Calendar(props: CalendarProps) {
+  const { value: propsValue, defaultValue, onChange } = props;
 
-  const {
+  const [mergedValue, setValue] = useMergeState(new Date(), {
     value: propsValue,
     defaultValue,
-    onChange
-  } = props;
-
-  const [value, setValue] = useState(() => {
-    // 通过判断 value 是不是 undefined 来区分受控模式和非受控模式
-    if (propsValue !== undefined) {
-      // 首次渲染，是受控模式
-      return propsValue;
-    } else {
-      return defaultValue;
-    }
   });
-
-  const isFirstRender = useRef(true);
-
-  // 受控模式
-  useEffect(() => {
-    if(propsValue === undefined && !isFirstRender.current) {
-      // 当不是首次渲染，但 value 变为 undefined 的情况，也就是从受控模式切换到了非受控模式，要同步设置 state 为 propsValue。
-      setValue(propsValue);
-    }
-    isFirstRender.current = false;
-  }, [propsValue]);
-
-  // 如果是受控模式，useState 的初始值设置 props.value，然后渲染用 props.value。
-  // 如果是非受控模式，那渲染用内部 state 的 value，然后 changeValue 里 setValue。
-  const mergedValue = propsValue === undefined ? value : propsValue;
 
   function changeValue(date: Date) {
     if (propsValue === undefined) {
-      // 非受控
       setValue(date);
     }
     onChange?.(date);
   }
 
-  return <div>
-    {mergedValue?.toLocaleDateString()}
-    <div onClick={()=> {changeValue(new Date('2024-5-1'))}}>2023-5-1</div>
-    <div onClick={()=> {changeValue(new Date('2024-5-2'))}}>2023-5-2</div>
-    <div onClick={()=> {changeValue(new Date('2024-5-3'))}}>2023-5-3</div>
-  </div>
+  return (
+    <div>
+      {mergedValue?.toLocaleDateString()}
+      <div
+        onClick={() => {
+          changeValue(new Date("2024-5-1"));
+        }}
+      >
+        2023-5-1
+      </div>
+      <div
+        onClick={() => {
+          changeValue(new Date("2024-5-2"));
+        }}
+      >
+        2023-5-2
+      </div>
+      <div
+        onClick={() => {
+          changeValue(new Date("2024-5-3"));
+        }}
+      >
+        2023-5-3
+      </div>
+    </div>
+  );
 }
 
 function App() {
-  return <Calendar defaultValue={new Date('2024-5-1')} onChange={(date) => {
-    console.log(date.toLocaleDateString());
-  }}/>
+  const [value, setValue] = useState(new Date("2024-5-1"));
+  // return <Calendar defaultValue={new Date('2024-5-1')} onChange={(date) => {
+  //   console.log(date.toLocaleDateString());
+  // }}/>
+  return (
+    <Calendar
+      value={value}
+      onChange={(date) => {
+        console.log(date.toLocaleDateString());
+        setValue(date);
+      }}
+    />
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/useMergeState.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from "react";
+
+function useMergeState<T>(
+  defaultStateValue: T,
+  props?: {
+    defaultValue?: T,
+    value?: T
+  }
+): [T, React.Dispatch<React.SetStateAction<T>>,] {
+  const { defaultValue, value: propsValue } = props || {};
+
+  const isFirstRender = useRef(true);
+
+  const [stateValue, setStateValue] = useState<T>(() => {
+    // 通过判断 value 是不是 undefined 来区分受控模式和非受控模式
+    if (propsValue !== undefined) {
+      return propsValue!;
+    } else if(defaultValue !== undefined){
+      return defaultValue!;
+    } else {
+      return defaultStateValue;
+    }
+  });
+
+  useEffect(() => {
+    // 当不是首次渲染，但 value 变为 undefined 的情况，也就是从受控模式切换到了非受控模式，要同步设置 state 为 propsValue
+    if(propsValue === undefined && !isFirstRender.current) {
+      setStateValue(propsValue!);
+    }
+
+    isFirstRender.current = false;
+  }, [propsValue]);
+
+  // 如果是受控模式，useState 的初始值设置 props.value，然后渲染用 props.value。
+  // 如果是非受控模式，那渲染用内部 state 的 value，然后 changeValue 里 setValue。
+  const mergedValue = propsValue === undefined ? stateValue : propsValue;
+
+  return [mergedValue, setStateValue]
+}
+
+export default useMergeState;
