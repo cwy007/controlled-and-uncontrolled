e6669589e4546942cf808a0e3ac4d2967a62790e
chanweiyan <chanweiyan007@gmail.com>
2024-12-18 11:30:14 +0800

    fix: 通过判断 value 是不是 undefined 来区分受控模式和非受控模式

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import { ChangeEvent, useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 interface CalendarProps{
-  value: Date;
+  value?: Date;
+  defaultValue?: Date;
   onChange?: (date: Date) => void;
 }
+
 function Calendar(props: CalendarProps) {
 
   const {
-    value,
+    value: propsValue,
+    defaultValue,
     onChange
   } = props;
 
+  const [value, setValue] = useState(() => {
+    // 通过判断 value 是不是 undefined 来区分受控模式和非受控模式
+    if (propsValue !== undefined) {
+      // 首次渲染，是受控模式
+      return propsValue;
+    } else {
+      return defaultValue;
+    }
+  });
+
+  const isFirstRender = useRef(true);
+
+  // 受控模式
+  useEffect(() => {
+    if(propsValue === undefined && !isFirstRender.current) {
+      // 非首次渲染，受控模式
+      setValue(propsValue);
+    }
+    isFirstRender.current = false;
+  }, [propsValue]);
+
+  // 如果是受控模式，useState 的初始值设置 props.value，然后渲染用 props.value。
+  // 如果是非受控模式，那渲染用内部 state 的 value，然后 changeValue 里 setValue。
+  const mergedValue = propsValue === undefined ? value : propsValue;
+
   function changeValue(date: Date) {
+    if (propsValue === undefined) {
+      // 非受控
+      setValue(date);
+    }
     onChange?.(date);
   }
 
   return <div>
-    {value.toLocaleDateString()}
+    {mergedValue?.toLocaleDateString()}
     <div onClick={()=> {changeValue(new Date('2024-5-1'))}}>2023-5-1</div>
     <div onClick={()=> {changeValue(new Date('2024-5-2'))}}>2023-5-2</div>
     <div onClick={()=> {changeValue(new Date('2024-5-3'))}}>2023-5-3</div>
@@ -24,11 +56,8 @@ function Calendar(props: CalendarProps) {
 }
 
 function App() {
-  const [value, setValue] = useState(new Date('2024-5-1'));
-
-  return <Calendar value={value} onChange={(date) => {
+  return <Calendar defaultValue={new Date('2024-5-1')} onChange={(date) => {
     console.log(date.toLocaleDateString());
-    setValue(date);
   }}/>
 }
 
