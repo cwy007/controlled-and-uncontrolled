ab3513860c2e5168c085c2840fbe90c91bb35893
chanweiyan <chanweiyan007@gmail.com>
2024-12-18 11:11:32 +0800

    fix: uncontrolled defaultValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from "react"
 
-function App() {
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
+interface CalendarProps{
+  defaultValue?: Date;
+  onChange?: (date: Date) => void;
+}
+function Calendar(props: CalendarProps) {
+
+  const {
+    defaultValue = new Date(),
+    onChange
+  } = props;
+
+  const [value, setValue] = useState(defaultValue);
+
+  function changeValue(date: Date) {
+    setValue(date);
+    onChange?.(date);
   }
 
-  return (
-    <input defaultValue="guang" onChange={onChange} />
-  )
+
+  return <div>
+    {value.toLocaleDateString()}
+    <div onClick={()=> {changeValue(new Date('2024-5-1'))}}>2023-5-1</div>
+    <div onClick={()=> {changeValue(new Date('2024-5-2'))}}>2023-5-2</div>
+    <div onClick={()=> {changeValue(new Date('2024-5-3'))}}>2023-5-3</div>
+  </div>
+}
+
+function App() {
+  return <Calendar defaultValue={new Date('2024-5-1')} onChange={(date) => {
+    console.log(date.toLocaleDateString());
+  }}/>
 }
 
 export default App
