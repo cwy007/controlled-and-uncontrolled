be2f52b51a33cfa06b8b7e76f86800adbbdde9e9
chanweiyan <chanweiyan007@gmail.com>
2024-12-21 11:07:01 +0800

    fix: Calendar forwardRef

--- src/components/CalendarTest/index.tsx
+++ src/components/CalendarTest/index.tsx
@@ -1,80 +1,95 @@
-import { useState } from 'react';
-import './index.module.scss';
-import { daysOfMonth, firstDayOfMonth, renderDates } from './utils';
-import classNames from 'classnames';
+import { ForwardedRef, forwardRef, useImperativeHandle, useState } from "react";
+import "./index.module.scss";
+import { daysOfMonth, firstDayOfMonth } from "./utils";
+import classNames from "classnames";
 
 const monthNames = [
-  '一月',
-  '二月',
-  '三月',
-  '四月',
-  '五月',
-  '六月',
-  '七月',
-  '八月',
-  '九月',
-  '十月',
-  '十一月',
-  '十二月',
-]
+  "一月",
+  "二月",
+  "三月",
+  "四月",
+  "五月",
+  "六月",
+  "七月",
+  "八月",
+  "九月",
+  "十月",
+  "十一月",
+  "十二月",
+];
 
 interface CalendarProps {
   defaultValue?: Date;
   onChange?: (date: Date) => void;
 }
 
-function Calendar(props: CalendarProps) {
-  const {
-    defaultValue = new Date(),
-    onChange,
-  } = props;
+export interface CalendarRef {
+  getDate: () => Date;
+  setDate: (date: Date) => void;
+}
+
+function InternalCalendar(props: CalendarProps, ref: ForwardedRef<CalendarRef>) {
+  const { defaultValue = new Date(), onChange } = props;
   const [date, setDate] = useState(defaultValue);
+
+  useImperativeHandle(ref, () => {
+    return {
+      getDate() {
+        return date;
+      },
+      setDate(newDate: Date) {
+        setDate(newDate);
+      }
+    }
+  }, [date]);
 
   const handlePrevMonth = () => {
     setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
-  }
+  };
 
   const handleNextMonth = () => {
     setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
-  }
+  };
 
   const renderDates = () => {
-  const days = [];
-  const daysCount = daysOfMonth(date.getFullYear(), date.getMonth());
-  const firstDay = firstDayOfMonth(date.getFullYear(), date.getMonth());
+    const days = [];
+    const daysCount = daysOfMonth(date.getFullYear(), date.getMonth());
+    const firstDay = firstDayOfMonth(date.getFullYear(), date.getMonth());
 
-  for (let i = 0; i < firstDay; i++) {
-    days.push(<div key={`empty-${i}`} className="empty" />);
-  }
-
-  for (let i = 1; i <= daysCount; i++) {
-    const handleClick = () => {
-      const currDate = new Date(date.getFullYear(), date.getMonth(), i);
-      setDate(currDate);
-      onChange?.(currDate);
+    for (let i = 0; i < firstDay; i++) {
+      days.push(<div key={`empty-${i}`} className="empty" />);
     }
-    days.push(
-      <div
-        key={i}
-        className={classNames({
-          day: true,
-          selected: i === date.getDate(),
-        })}
-        onClick={handleClick}
-      >
-        {i}
-      </div>
-    );
-  }
 
-  return days;
-};
+    for (let i = 1; i <= daysCount; i++) {
+      const handleClick = () => {
+        const currDate = new Date(date.getFullYear(), date.getMonth(), i);
+        setDate(currDate);
+        onChange?.(currDate);
+      };
+      days.push(
+        <div
+          key={i}
+          className={classNames({
+            day: true,
+            selected: i === date.getDate(),
+          })}
+          onClick={handleClick}
+        >
+          {i}
+        </div>
+      );
+    }
+
+    return days;
+  };
 
   return (
     <div className="calendar">
       <div className="header">
         <button onClick={handlePrevMonth}>&lt;</button>
-        <div>{date.getFullYear()}年{monthNames[date.getMonth()]}</div>
+        <div>
+          {date.getFullYear()}年{monthNames[date.getMonth()]}
+        </div>
         <button onClick={handleNextMonth}>&gt;</button>
       </div>
       <div className="days">
@@ -86,10 +101,10 @@ function Calendar(props: CalendarProps) {
         <div className="day">五</div>
         <div className="day">六</div>
 
-        {renderDates(date)}
+        {renderDates()}
       </div>
     </div>
   );
 }
 
-export default Calendar;
+export default forwardRef<CalendarRef, CalendarProps>(InternalCalendar);;
--- src/pages/CalendarTest/index.tsx
+++ src/pages/CalendarTest/index.tsx
@@ -1,12 +1,35 @@
-import Calendar from '../../components/CalendarTest'
+import { useEffect, useRef } from "react";
+import Calendar, { CalendarRef } from "../../components/CalendarTest";
 
 const CalendarTest = () => {
+  const calendarRef = useRef<CalendarRef>(null);
+
+  useEffect(() => {
+    console.log('-->1', calendarRef.current?.getDate().toLocaleDateString());
+
+    setTimeout(() => {
+      calendarRef.current?.setDate(new Date(2024, 11, 25));
+      // console.log('-->2', calendarRef.current?.getDate().toLocaleDateString());
+    }, 3000)
+
+    setTimeout(() => {
+      // calendarRef.current?.setDate(new Date(2024, 11, 25));
+      console.log('-->2', calendarRef.current?.getDate().toLocaleDateString());
+    }, 5000)
+  }, [])
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: 16 }}>
-      <Calendar defaultValue={new Date('2024-1-1')} />
-      <Calendar defaultValue={new Date('2024-12-21')} />
+    <div style={{ display: "flex", flexDirection: "column", gap: 16 }}>
+      <Calendar
+        ref={calendarRef}
+        defaultValue={new Date("2024-1-1")}
+        onChange={(date) => {
+          console.log(date.toLocaleString());
+        }}
+      />
+      <Calendar defaultValue={new Date("2024-12-21")} />
     </div>
-  )
-}
+  );
+};
 
 export default CalendarTest;
